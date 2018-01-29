510d502663a3bc2c997e5a9781a18fdf2a241ef1
yuri <930502900@qq.com>
2018-01-29 08:14:13 +0800

    20180129

---
+++ Bigcake/Project/Root/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Bigcake
+//
+//  Created by 武国斌 on 2018/1/29.
+//  Copyright © 2018年 武国斌. All rights reserved.
+//
+
+#import "BCPTabBarController.h"
+
+@interface RootViewController : BCPTabBarController
+
+@end
---
+++ Bigcake/Project/Root/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  RootViewController.m
+//  Bigcake
+//
+//  Created by 武国斌 on 2018/1/29.
+//  Copyright © 2018年 武国斌. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
